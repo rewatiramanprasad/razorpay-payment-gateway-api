8e3e264eca855ab781286170c7d2ee072f8902ab
Raman Prasad <ramanprasad.0203@gmail.com>
2025-07-17 00:27:23 +0530

    fix: format

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-  transform: {
-    '^.+\\.js$': 'babel-jest'
-  },
-  testEnvironment: 'node'
+	transform: {
+		"^.+\\.js$": "babel-jest",
+	},
+	testEnvironment: "node",
 };
--- src/__tests__/health.test.js
+++ src/__tests__/health.test.js
@@ -1,8 +1,9 @@
-import request from 'supertest';
-import {app} from '../../app.js'; 
-test('GET /v1/api/health should return server health', async () => {
-  const res=await request(app).get('/v1/api/health');
-    expect(res.statusCode).toBe(200);
-  expect(res.body).toHaveProperty('message', 'server is healthy');
-  console.log(res.body)
-})
+import request from "supertest";
+import { app } from "../../app.js";
+
+test("GET /v1/api/health should return server health", async () => {
+	const res = await request(app).get("/v1/api/health");
+	expect(res.statusCode).toBe(200);
+	expect(res.body).toHaveProperty("message", "server is healthy");
+	console.log(res.body);
+});
