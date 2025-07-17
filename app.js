fb327f5fb4cb3cc388432ecd84a3d82875987d8c
Raman Prasad <ramanprasad.0203@gmail.com>
2025-07-17 10:07:12 +0530

    fix: environment variable

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ import dotenv from "dotenv";
 import express from "express";
 import Razorpay from "razorpay";
 import { dbSeeding } from "./src/db/seeding.js";
+import { paymentRouter } from "./src/route.js";
 
 dotenv.config({ path: "./config.env" });
 export const app = express();
 
-import { paymentRouter } from "./src/route.js";
 
 dbSeeding();
 
 app.use(express.json());
-console.log(process.env.RAZORPAY_KEY_ID);
 
 export const instance = new Razorpay({
 	key_id: process.env.RAZORPAY_KEY_ID,
